741d1f5556e3e883f97303204dee9a12f4c0bc55
BarTes8 <barbara.tessema@gmail.com>
2021-05-24 15:51:46 +0200

    did some tasks

--- 02-types/tasks/04-sharedPtr.cpp
+++ 02-types/tasks/04-sharedPtr.cpp
@@ -6,10 +6,21 @@
 // It should also display the value of this int and the number of how many pointers are pointing to it - use `use_count()`.
 // Display the same information in main() before and after calling foo()
 
+void foo(std::shared_ptr<int> p) {
+    std::cout << *std::make_shared<int>(20) << std::endl;
+}
+
+void print(std::shared_ptr<int> p){
+    std::cout << *p << std::endl;
+    std::cout << p.use_count() << std::endl;
+}
+
 int main() {
     std::shared_ptr<int> number = std::make_shared<int>(10);
     // display the value under number pointer and use_count() of it
+    print(number);
     foo(number);
+    print(number);
     // display the value under number pointer and use_count() of it
 
     return 0;
--- 02-types/tasks/05-calculateProduct.cpp
+++ 02-types/tasks/05-calculateProduct.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <memory>
 
 // TODO: Implement calculateProduct()
 // It should take 2 int values and return their product as a shared_ptr.
 // Additionally, check how many owners are there.
+std::shared_ptr<int> calculateProduct(int a, int b) {
+    auto ptr = std::make_shared<int>(a * b);
+    return ptr;
+}
 
 int main() {
     auto number = calculateProduct(10, 20);
